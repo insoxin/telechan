cbae114851c10f681ff630a3600aea80a4df7ef5
Redy <19371836+insoxin@users.noreply.github.com>
2022-03-13 02:35:24 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -7,16 +7,6 @@ const replyToMessage = (ctx: any, messageId: string, string: string) =>
     reply_to_message_id: messageId,
   });
 
-const sendkey = () => (ctx: any) => {
 
-  const messageId = ctx.message.message_id;
-  const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
-  const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
-
-  const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
-
-  replyToMessage(ctx, messageId, ``);
-  // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
-};
 
 export { sendkey };
